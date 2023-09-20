ab93f06196020af5dba849656d6dbd911ecf503b
Harshita Saraswat <harshitasaraswat20@gmail.com>
2023-09-20 11:38:01 +0530

    pivot_element

---
+++ pivot_element.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int max_pivot_element(int arr[],int n){
+    int start=0;
+    int end=n-1;
+    int mid=(start+end)/2;
+    
+    while(start<=end){
+        if(start==end){
+            return start;
+        }
+        if(arr[mid]>arr[mid+1]){
+            return mid;
+        }
+        if(arr[mid]<arr[mid-1]){
+            return mid-1;
+        }
+        if(start>mid){
+            end=mid-1;
+        }
+        if (start<mid){
+            start=mid+1;
+        }
+        mid=(start+end)/2;
+    }
+    return -1;
+
+
+}
+
+int main(){
+    int n=8;
+    int arr[]={14,15,16,18,2,4,6,8};
+    int ans=max_pivot_element(arr,n);
+    cout<<"the pivot element is: "<<ans<<endl;
+
+    
+
+    return 0;
+}
