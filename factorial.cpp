7ece7349db0c726dff4be086a4a29516d92a6c6e
Harshita Saraswat <harshitasaraswat20@gmail.com>
2023-09-22 23:46:41 +0530

    factorial

--- factorial.cpp
+++ factorial.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main(){
     int n=1;
-    int factorial=1;
+    int factorial_finding=1;
     for(int i=1;i<=n;i++){
-        factorial=factorial*i;   
+        factorial_finding=factorial_finding*i;   
     }
-    cout<<factorial<<endl;
+    cout<<factorial_finding<<endl;
 }
