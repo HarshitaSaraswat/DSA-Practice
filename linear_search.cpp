76f98d082f46ed3da33a422e672afef849d363aa
Harshita Saraswat <harshitasaraswat20@gmail.com>
2023-09-22 23:48:48 +0530

    linear_search

--- linear_search.cpp
+++ linear_search.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void linear_search(int arr[],int n,int target){
+void linear_search1(int arr[],int n,int target){
     
     for(int i=0;i<n;i++){
         if(arr[i]==target){
@@ -14,6 +14,6 @@ int main(){
     int n=5;
     int arr1[5]={1,2,3,4,5};
     int target=4;
-    linear_search(arr1, n, target);
+    linear_search1(arr1, n, target);
     return 0;
 }
